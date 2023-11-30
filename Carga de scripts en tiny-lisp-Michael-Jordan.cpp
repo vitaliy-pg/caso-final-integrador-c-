3cd08a67c3cd0eca425d5af2f08dcf5f241ba8ad
vitaliy-pg <aljndrvt@myuax.com>
2023-11-30 12:04:49 +0100

    defino los colores de la consola

--- Carga de scripts en tiny-lisp-Michael-Jordan.cpp
+++ Carga de scripts en tiny-lisp-Michael-Jordan.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
+using namespace std;
+struct ColorConsole
+{
+    static constexpr auto fg_blue = "\033[34m";
+    static constexpr auto bg_white = "\033[47m";
+};
