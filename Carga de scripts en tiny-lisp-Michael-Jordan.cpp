3c1e69fe45e465b5d2ed589a61d218bf40426367
vitaliy-pg <aljndrvt@myuax.com>
2023-11-30 12:05:24 +0100

    muestro la consola y manejo le texto

--- Carga de scripts en tiny-lisp-Michael-Jordan.cpp
+++ Carga de scripts en tiny-lisp-Michael-Jordan.cpp
@@ -7,3 +7,8 @@ struct ColorConsole
     static constexpr auto fg_blue = "\033[34m";
     static constexpr auto bg_white = "\033[47m";
 };
+struct ConsoleBox
+{
+    void new_text() {/*...*/}
+    void set_text(const string &text) { cout << text << endl; }
+};
