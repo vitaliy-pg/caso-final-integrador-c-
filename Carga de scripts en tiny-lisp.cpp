fafb53b4eeb0f4ec8d5d72525d39e54f87b4160a
vitaliy-pg <aljndrvt@myuax.com>
2023-11-30 11:47:08 +0100

    muestra el cotenido del archivo si es verdadero el script

--- Carga de scripts en tiny-lisp.cpp
+++ Carga de scripts en tiny-lisp.cpp
@@ -6,4 +6,8 @@ void load_script(const char* filename, bool show_script = false) {
     std::ifstream file(filename);
     if (file.is_open()) {
         std::string line;
+        if (show_script) {
+            std::cout << "Contenido del archivo '" << filename << "':" << std::endl;
+            while (std::getline(file, line)) {
+                std::cout << line << std::endl;
 }
